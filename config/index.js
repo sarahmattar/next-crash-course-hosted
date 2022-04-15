699398adde4296b8fc4451106fd8f194adb44295
Sarah Mattar <sarah.mattar@commit.dev>
2022-04-15 13:58:32 -0700

    Update server config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
+export const server = dev ? 'http://localhost:3000' : 'https://next-crash-course-hosted-1jl9ka0c8-sarahmattar.vercel.app'
