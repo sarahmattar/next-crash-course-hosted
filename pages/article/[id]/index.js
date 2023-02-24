c1f5c280ad38d85277534c90c1ead4abe404f835
Sarah Mattar <sarah.mattar@mac.com>
2023-02-23 21:36:38 -0800

    Remove commented code

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
 import { articles } from '../../../data'
-// import { useRouter } from 'next/router'
 import Meta from '../../../components/Meta'
 
 const article = ({ article }) => {
-  // this will contain any params in the route
-  // const router = useRouter();
-  // const { id } = router.query;
-
   return (
     <>
       <Meta
@@ -27,7 +22,6 @@ export const getStaticProps = context => {
   const { id } = context.params
   const filtered = articles.filter(article => article.id === id)
   const article = filtered[0]
-  console.log(article)
 
   return {
     props: {
@@ -47,31 +41,5 @@ export const getStaticPaths = async () => {
     fallback: false,
   }
 }
-
-// export const getStaticProps = async (context) => {
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
-//     const article = await res.json()
-
-//     return {
-//         props: {
-//             article
-//         }
-//     }
-// }
-
-// export const getStaticPaths = async () => {
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-//     const articles = await res.json()
-
-//     const ids = articles.map(article => article.id)
-//     const paths = ids.map(id => ({
-//         params: { id: id.toString() }
-//     }))
-
-//     return {
-//         paths,
-//         fallback: false
-//     }
-// }
 
 export default article
